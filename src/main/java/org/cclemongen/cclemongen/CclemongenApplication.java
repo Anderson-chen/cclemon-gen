b05732cc91e1b3691a55b6524126d9ef5359c31c
Anderson-chen <55676457+Anderson-chen@users.noreply.github.com>
2022-09-24 18:14:53 +0800

    Add files via upload
    init

---
+++ src/main/java/org/cclemongen/cclemongen/CclemongenApplication.java
@@ -0,0 +1,13 @@
+package org.cclemongen.cclemongen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CclemongenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CclemongenApplication.class, args);
+	}
+
+}
