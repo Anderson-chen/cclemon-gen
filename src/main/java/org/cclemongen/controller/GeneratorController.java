1822ce9ce151e74c1f301354de3759192645882d
andychen <z60509561@gmail.com>
2024-03-25 14:27:01 +0800

    PG-maven to gradle

--- src/main/java/org/cclemongen/controller/GeneratorController.java
+++ src/main/java/org/cclemongen/controller/GeneratorController.java
@@ -2,8 +2,6 @@ package org.cclemongen.controller;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletResponse;
-
 import org.cclemongen.dto.FreeMakerGenDTO;
 import org.cclemongen.service.MetaDataService;
 import org.springframework.http.HttpStatus;
@@ -26,73 +24,77 @@ import lombok.extern.slf4j.Slf4j;
 @AllArgsConstructor
 public class GeneratorController {
 
-   private final  MetaDataService metaDataService;
+    private final MetaDataService metaDataService;
 
-   @GetMapping(value = "/home")
-   public String home(HttpServletResponse res) {
-      return "generator.html";
-   }
+    @GetMapping(value = "/home")
+    public String home() {
+        return "generator.html";
+    }
 
-   @PostMapping("/codeGen")
-   @ResponseBody
-   public Response codeGen(@RequestBody Request req) throws Exception {
+    @PostMapping("/codeGen")
+    @ResponseBody
+    public Response codeGen(@RequestBody Request req) throws Exception {
 
-      log.info("req : {} ", req);
+        log.info("req : {} ", req);
 
-      FreeMakerGenDTO freeMakerGenDTO = FreeMakerGenDTO.builder().schema(req.getSchema()).tableName(req.getTableName())
-            .destination(req.getDestination())
-            .groupId(req.getGroupId()).build();
+        FreeMakerGenDTO freeMakerGenDTO = FreeMakerGenDTO.builder().schema(req.getSchema()).tableName(req.getTableName())
+                .destination(req.getDestination())
+                .groupId(req.getGroupId()).build();
 
-      metaDataService.codeGen(freeMakerGenDTO);
+        metaDataService.codeGen(freeMakerGenDTO);
 
-      return Response.builder().result(req).code(200).msg("產檔成功!").build();
+        return Response.builder().result(req).code(200).msg("產檔成功!").build();
 
-   }
+    }
 
-   @GetMapping("/getAllSchema")
-   @ResponseBody
-   public Response getAllSchema() throws Exception {
+    @GetMapping("/getAllSchema")
+    @ResponseBody
+    public Response getAllSchema() throws Exception {
 
-      List<String> schemas = metaDataService.getAllSchema();
+        List<String> schemas = metaDataService.getAllSchema();
 
-      return Response.builder().result(schemas).code(200).msg("查詢成功").build();
+        return Response.builder().result(schemas).code(200).msg("查詢成功").build();
 
-   }
+    }
 
-   @GetMapping("/getTableNames")
-   @ResponseBody
-   public Response getTableNames(@RequestParam("schema") String schema) throws Exception {
+    @GetMapping("/getTableNames")
+    @ResponseBody
+    public Response getTableNames(@RequestParam("schema") String schema) throws Exception {
 
-      log.info("schema : {} ", schema);
+        log.info("schema : {} ", schema);
 
-      List<String> tableNames = metaDataService.getTableNames(schema);
+        List<String> tableNames = metaDataService.getTableNames(schema);
 
-      return Response.builder().result(tableNames).code(200).msg("查詢成功").build();
+        return Response.builder().result(tableNames).code(200).msg("查詢成功").build();
 
-   }
+    }
 
-   @ExceptionHandler(Exception.class)
-   public ResponseEntity<Response> handleException(Exception e) {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Response> handleException(Exception e) {
 
-      log.error("{}", e.getMessage());
+        log.error("{}", e.getMessage());
 
-      return new ResponseEntity<Response>(Response.builder().code(200).msg("執行失敗!").build(), null, HttpStatus.OK);
-   }
+        return new ResponseEntity<Response>(Response.builder().code(200).msg("執行失敗!").build(), null, HttpStatus.OK);
+    }
+
+    @Data
+    public static class Request {
+        private String schema;
+        private String tableName;
+        private String destination;
+        private String groupId;
+    }
+
+    @Data
+    @Builder
+    public static class Response {
+        private int code;
+        private String msg;
+        private Object result;
+    }
 
 }
 
-@Data
-class Request {
-   private String schema;
-   private String tableName;
-   private String destination;
-   private String groupId;
-}
 
-@Data
-@Builder
-class Response {
-   private int code;
-   private String msg;
-   private Object result;
-}
+
+
--- src/test/java/org/cclemongen/cclemongen/CclemongenApplicationTests.java
+++ src/test/java/org/cclemongen/cclemongen/CclemongenApplicationTests.java
@@ -1,13 +1,33 @@
 package org.cclemongen.cclemongen;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+import org.cclemongen.controller.GeneratorController;
+import org.cclemongen.service.MetaDataService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(GeneratorController.class)
 class CclemongenApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private MetaDataService service;
+
 	@Test
-	void contextLoads() {
+	void shouldReturnDefaultMessage() throws Exception {
+		this.mockMvc.perform(get("/home")).andDo(print()).andExpect(status().isOk());
+
+		// this.mockMvc.perform(get("/getAllSchema")).andDo(print()).andExpect(status().isOk());
+
 	}
 
 }
+
